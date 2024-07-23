afe2e2bc941c81e4fd2865a8489bfa04bc7b572a
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2024-07-23 10:23:50 +0200

    set example added

---
+++ Fundamentals/src/oopBasic/SortedMapExample.java
@@ -0,0 +1,21 @@
+package oopBasic;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapExample {
+	
+	public static void main(String[] args) {
+		SortedMap<String, Integer> numbers = new TreeMap<String, Integer>();
+		
+		numbers.put("One", 1);
+		numbers.put("Two", 2);
+		
+		System.out.println(numbers);
+		System.out.println(numbers.firstKey());
+		System.out.println(numbers.lastKey());
+		System.out.println(numbers.remove("One"));
+		System.out.println(numbers);
+	}
+
+}
